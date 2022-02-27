6b83c226d8a8c5aa9f73b0ed3b1a5b949d7dfb86
Bahriddin <Bahriddin@gmail.com>
2022-02-27 12:43:40 +0300

    script

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const ismingiz = prompt("Ismingizni kiriting")
+const familyangiz = prompt("Familyangizmi kiriting")
+const tugilganyil = prompt("tugilgan yilingizni kiriting")
+const hozirgiyil = 2022
+const yoshi = hozirgiyil - tugilganyil 
+alert("Ismingiz: " + ismingiz + ", Familyangiz: " + familyangiz + ", Yosingiz: " + yoshi)
+console.log("ismingiz: " + ismingiz  + ", familyangiz: " + familyangiz + ", yosingiz: " + yoshi);
